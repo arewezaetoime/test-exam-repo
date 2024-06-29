71e1af1b2b194e0a6f3701f1f76bb539bdd400ba
Hristo Iliev (A1 BG) <Hristo.Iliev@a1.bg>
2024-06-29 13:12:58 +0300

    fxed the ui tests

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Check about page', async ({ page }) => {
+test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
